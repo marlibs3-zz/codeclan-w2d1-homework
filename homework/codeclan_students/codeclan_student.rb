e607017cf6bf750ba3259c14403d2775c721ac5b
martabeveridge <marta@beveridge.uk>
2018-03-12 16:38:03 +0000

    part a of homework completed

---
+++ homework/codeclan_students/codeclan_student.rb
@@ -0,0 +1,37 @@
+class Student
+  attr_accessor :student_name, :student_cohort, :student_words, :student_favourite_language
+
+  def initialize(input_student_name, input_student_cohort)
+    @student_name = input_student_name
+    @student_cohort = input_student_cohort
+    @student_words = ""
+    @student_favourite_language = ""
+  end
+
+  def return_student_name
+    return @student_name
+  end
+
+  def return_student_cohort
+    return @student_cohort
+  end
+
+  def amend_student_name(new_student_name)
+    @student_name = new_student_name
+  end
+
+  def amend_student_cohort(new_student_cohort)
+    @student_cohort = new_student_cohort
+  end
+
+  def make_student_speak(student_words)
+    @student_words = student_words
+    return @student_words
+  end
+
+  def give_student_favourite_language(student_favourite_language)
+    @student_favourite_language = student_favourite_language
+    return @student_favourite_language
+  end
+
+end
